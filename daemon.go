f3653fc17bc1d8fbc63170040e7aefc647201b7f
stdfox <stdfox@protonmail.com>
2019-05-10 09:01:27 +0300

    Added daemon json-rpc client.

---
+++ daemon.go
@@ -0,0 +1,80 @@
+package xmrrpc
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"math/rand"
+	"net/http"
+)
+
+type DaemonClient struct {
+	endpoint string
+	username string
+	password string
+}
+
+type jsonRPCRequest struct {
+	Version string      `json:"jsonrpc"`
+	ID      uint64      `json:"id"`
+	Method  string      `json:"method"`
+	Params  interface{} `json:"params"`
+}
+
+type jsonRPCResponse struct {
+	Version string           `json:"jsonrpc"`
+	ID      uint64           `json:"id"`
+	Result  *json.RawMessage `json:"result"`
+	Error   jsonRPCError     `json:"error"`
+}
+
+type jsonRPCError struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+func NewDaemonClient(endpoint string, username string, password string) *DaemonClient {
+	return &DaemonClient{endpoint: endpoint, username: username, password: password}
+}
+
+func (dc *DaemonClient) jsonRPCRequest(method string, params interface{}, reply interface{}) error {
+	jsonRPCRequest := &jsonRPCRequest{
+		Version: "2.0",
+		ID:      rand.Uint64(),
+		Method:  method,
+		Params:  params,
+	}
+
+	jsonRPCRequestBody, err := json.Marshal(jsonRPCRequest)
+	if err != nil {
+		return err
+	}
+
+	httpRequest, err := http.NewRequest(http.MethodPost, dc.endpoint, bytes.NewReader(jsonRPCRequestBody))
+	if err != nil {
+		return err
+	}
+	httpRequest.Header.Set("Content-Type", "application/json")
+
+	httpClient := &http.Client{}
+	httpResponse, err := httpClient.Do(httpRequest)
+	if err != nil {
+		return err
+	}
+	defer httpResponse.Body.Close()
+
+	jsonRPCResponse := &jsonRPCResponse{}
+	if err := json.NewDecoder(httpResponse.Body).Decode(&jsonRPCResponse); err != nil {
+		return err
+	}
+
+	if jsonRPCResponse.Error.Code < 0 {
+		return errors.New(jsonRPCResponse.Error.Message)
+	}
+
+	if jsonRPCResponse.Result == nil {
+		return errors.New("Unexpected null result")
+	}
+
+	return json.Unmarshal(*jsonRPCResponse.Result, reply)
+}
