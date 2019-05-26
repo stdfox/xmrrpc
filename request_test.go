0a643f2136c1a9014363cea7fad33768fd642d51
stdfox <stdfox@protonmail.com>
2019-05-26 17:46:28 +0300

    Fixed request tests.

--- request_test.go
+++ request_test.go
@@ -12,10 +12,10 @@ func TestRandomKey(t *testing.T) {
 	assert.Len(t, k, 12, "Key length is incorrect.")
 
 	d, err := base64.StdEncoding.DecodeString(k)
-	assert.Error(t, err, "Key encode is incorrect.")
+	assert.NoError(t, err, "Key encode is incorrect.")
 	assert.Len(t, d, 8, "Key bytes length is incorrect.")
 }
 
 func TestH(t *testing.T) {
-	assert.Equal(t, h("1234567890"), "e807f1fcf82d132f9bb018ca6738a19f1", "MD5 hash is incorrect.")
+	assert.Equal(t, h("1234567890"), "e807f1fcf82d132f9bb018ca6738a19f", "MD5 hash is incorrect.")
 }
