1c8a12fcd9312f6c80a4e9fb20cf7f93e9515d47
stdfox <stdfox@protonmail.com>
2019-05-26 20:39:24 +0300

    Updated request tests.

--- request_test.go
+++ request_test.go
@@ -2,6 +2,8 @@ package xmrrpc
 
 import (
 	"encoding/base64"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -22,4 +24,23 @@ func TestRandomKey(t *testing.T) {
 func TestH(t *testing.T) {
 	assert.NotPanics(t, func() { h("") })
 	assert.Equal(t, h("1234567890"), "e807f1fcf82d132f9bb018ca6738a19f", "MD5 hash is incorrect.")
+}
+
+func TestRequest(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("WWW-authenticate", `Digest qop="auth",algorithm=MD5,realm="monero-rpc",nonce="jmk3hzH2xpPmUBSD0uy+uQ==",stale=false`)
+		w.Header().Add("WWW-authenticate", `Digest qop="auth",algorithm=MD5-sess,realm="monero-rpc",nonce="jmk3hzH2xpPmUBSD0uy+uQ==",stale=false`)
+
+		if r.Header.Get("Authorization") == "" {
+			w.WriteHeader(http.StatusUnauthorized)
+		} else {
+			w.WriteHeader(http.StatusOK)
+		}
+	}))
+	defer ts.Close()
+
+	res, err := request(http.MethodPost, ts.URL, nil, "username", "password")
+	if assert.NoError(t, err) {
+		assert.Equal(t, http.StatusOK, res.StatusCode)
+	}
 }
